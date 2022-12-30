8b3b3d2039af8e5b150dc26acc68f8b6aff103b0
Leona Maroni <dev@leona.is>
2022-12-30 12:54:09 +0000

    remove accounts/preferences from urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,9 +5,9 @@ from accounts.views import auth, preferences
 app_name = "accounts"
 urlpatterns = [
     path("login/", auth.LoginView.as_view(), name="login"),
-    path(
-        "preferences/",
-        preferences.PreferencesIndexView.as_view(),
-        name="preferences_index",
-    ),
+    # path(
+    #     "preferences/",
+    #     preferences.PreferencesIndexView.as_view(),
+    #     name="preferences_index",
+    # ),
 ]
