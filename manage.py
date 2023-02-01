7a72d49413ac4c0cd53873e83964f6ac6977d882
Leona Maroni <dev@leona.is>
2023-02-01 22:10:25 +0000

    meta: fix shebang in manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/nix/store/zdba9frlxj2ba8ca095win3nphsiiqhb-python3-3.10.8/bin/python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nomsable.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nomsable.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
